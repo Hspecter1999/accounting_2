562b535191f007bb74f3c7095b0ee8d2fd076f45
Saksham <saqsham@gmail.com>
2019-08-05 23:13:53 +0530

    db status and Joi

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
+// const express = require('express')
+// const app = express()
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open',() => console.log('connected to databse')) // confirms that db is connected
+
+// app.use(express.json())
+// route ecample 
+// const subsRouter = require('./routes/subscribers')
+// app.use('/subscribers', subsRouter)
